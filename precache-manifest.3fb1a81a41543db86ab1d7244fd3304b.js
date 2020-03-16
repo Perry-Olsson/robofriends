6887b5ef99b11bdf768785d5d3f13bd76d98f70b
Perry Olsson <olsson.perry@gmail.com>
2020-03-16 15:48:51 -0700

    Updates

---
+++ precache-manifest.3fb1a81a41543db86ab1d7244fd3304b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78a8d9cd9e70323dea39d54c12dad320",
+    "url": "/Perry-Olsson/robofriends/index.html"
+  },
+  {
+    "revision": "978d3e6abce16fc74e6c",
+    "url": "/Perry-Olsson/robofriends/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "e7366f11079b094b442c",
+    "url": "/Perry-Olsson/robofriends/static/css/main.03374b79.chunk.css"
+  },
+  {
+    "revision": "978d3e6abce16fc74e6c",
+    "url": "/Perry-Olsson/robofriends/static/js/2.faf57d51.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Perry-Olsson/robofriends/static/js/2.faf57d51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7366f11079b094b442c",
+    "url": "/Perry-Olsson/robofriends/static/js/main.bc5b99df.chunk.js"
+  },
+  {
+    "revision": "0858e993c6a049bedac7",
+    "url": "/Perry-Olsson/robofriends/static/js/runtime-main.7d56b03b.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Perry-Olsson/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Perry-Olsson/robofriends/precache-manifest.3fb1a81a41543db86ab1d7244fd3304b.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Perry-Olsson/robofriends/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
